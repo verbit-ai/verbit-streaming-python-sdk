1abf4de79183ae96b60e61513e59e6a605f07c69
EliAsor <tcfh.ea@gmail.com>
2022-01-16 19:11:02 +0200

    RND-12951 - adapting client to new server communication format

--- examples/example_client.py
+++ examples/example_client.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 
-import wave
 import argparse
-from math import ceil
 from time import sleep
 from pathlib import Path
 
-from verbit.streaming_client import SpeechStreamClient, MediaConfig
+from verbit.streaming_client import SpeechStreamClient
 
 # constants
 CHUNK_DURATION_SECONDS = 0.1
 
 
-# Warning: Server only supports: mono, samplewidth == 2, signed-16-bit audio.
-def media_generator_wavefile(media_path, chunk_seconds):
+def media_generator_wavefile(wav_path, chunk_duration):
     """
     Example generator, for streaming a 'WAV' audio-file, simulating realtime playback-rate using sleep()
     """
-    with wave.open(str(media_path), 'rb') as wav:
-        nchannels, samplewidth, sample_rate, nframes, _, _ = wav.getparams()
-        samples_per_chunk = ceil(chunk_seconds * sample_rate)
-        chunk_bytes = wav.readframes(samples_per_chunk)
-        while chunk_bytes:
+
+    # calculate chunk duration
+    # Note: assuming input file is a 16-bit mono 16000Hz WAV file
+    chunk_size = int(chunk_duration * 2 * 16000)
+
+    with open(str(wav_path), 'rb') as wav:
+        while chunk_bytes := wav.read(chunk_size):
             yield chunk_bytes
-            chunk_bytes = wav.readframes(samples_per_chunk)
-            sleep(chunk_seconds)
+            sleep(chunk_duration)
 
 
 if __name__ == '__main__':
@@ -32,29 +30,23 @@ if __name__ == '__main__':
     # read command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--media_path', required=True, type=Path, help='Full path of the media file to stream')
-    parser.add_argument('-s', '--stream_id', required=True, type=str, help='Stream ID obtained via booking a session')
     args = parser.parse_args()
 
     # set access token
     access_token = 'secret!'
 
     # init verbit streaming client
-    client = SpeechStreamClient(access_token=access_token, stream_id=args.stream_id)
-
-    # init media config
-    media_config = MediaConfig(
-        format='PCM',
-        sample_rate=16000,
-        sample_width=2,
-        num_channels=1
-    )
+    client = SpeechStreamClient(access_token=access_token)
 
     # init media chunks generator
     media_generator = media_generator_wavefile(args.media_path, CHUNK_DURATION_SECONDS)
 
     # upgrade connection to websocket and start audio stream
+    print('Connecting ...')
     response_generator = client.start_stream(media_generator=media_generator)
+    print('Connected!')
 
     # get transcription responses
+    print('Listening for responses ...')
     for response in response_generator:
         print(response)
