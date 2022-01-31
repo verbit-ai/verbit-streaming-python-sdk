03558c9b5d67285386c2a3cba81f008778f4dde3
Kobi Calev <kobi.calev@verbit.ai>
2022-01-31 19:39:08 +0200

    draft

--- tests/test_example_client.py
+++ tests/test_example_client.py
@@ -2,8 +2,8 @@
 # import json
 # import struct
 import unittest
-# import websocket
-# import pkg_resources
+import websocket
+import pkg_resources
 # from os import path
 # from unittest.mock import MagicMock, patch
 # from tenacity import RetryError
@@ -13,17 +13,17 @@ import unittest
 from examples import example_client
 
 
-# # Load test resources:
-# def _load_json_resource(name):
-#     rel_path = pkg_resources.resource_filename(__name__, path.join('resources', name + '.json'))
-#     with open(rel_path, 'rb') as f:
-#         json_bytes = f.read()
-#     return json_bytes
+# Load test resources:
+def _load_json_resource(name):
+    rel_path = pkg_resources.resource_filename(__name__, path.join('resources', name + '.json'))
+    with open(rel_path, 'rb') as f:
+        json_bytes = f.read()
+    return json_bytes
 
 
-# RESOURCE_KEYS = ['happy_json_resp0', 'happy_json_resp1', 'happy_json_resp_EOS']
-# # init mock responses
-# RESPONSES = {k: _load_json_resource(k) for k in RESOURCE_KEYS}
+RESOURCE_KEYS = ['happy_json_resp0', 'happy_json_resp1', 'happy_json_resp_EOS']
+# init mock responses
+RESPONSES = {k: _load_json_resource(k) for k in RESOURCE_KEYS}
 
 
 # # -----------
