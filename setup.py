6dcc58d794e9adba96ad1161bbbf7444081d952a
EliAsor <tcfh.ea@gmail.com>
2022-02-09 11:34:14 +0200

    RND-11553 - remove sub-patch version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/verbit-ai/verbit-streaming-python-sdk',
 
-    version='0.8.0.3',
+    version='0.8.0',
 
     packages=['verbit'],
     package_dir={'verbit': 'verbit'},
