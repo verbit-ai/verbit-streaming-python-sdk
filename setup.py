63e2646c3d91c67235592c1cd9688bdb53f6d5aa
Eli Asor <EliAsor@users.noreply.github.com>
2022-02-09 13:09:38 +0200

    Adding PyPI Info (#8)

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from setuptools import setup
 
 setup(
     name='verbit-streaming-sdk',
-    description='',
-    long_description='',
+    description="Client SDK for Verbit's Streaming Speech Recognition services",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/verbit-ai/verbit-streaming-python-sdk',
+
     version='0.8.0',
 
     packages=['verbit'],
     package_dir={'verbit': 'verbit'},
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
-        'websocket-client==1.2.3',
-        'tenacity==8.0.1'
+        'websocket-client~=1.2.3',
+        'tenacity~=8.0.1'
     ],
     zip_safe=False
 )
