2eea07609aac7df8ed449e793400392f0bd01fbe
Shani Zohar <86555191+shanizo@users.noreply.github.com>
2023-06-28 11:28:19 +0300

    Update tenacity requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         'websocket-client~=1.2.3',
-        'tenacity~=8.0.1',
+        'tenacity<9>8',
         'requests<3'
     ],
     zip_safe=False
